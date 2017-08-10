379abed19c8b6d9e264b9ae9f2adf705fc64fc1b
Rodolpho Niceto <rodolphoniceto@yahoo.com.br>
2017-08-10 11:39:13 -0300

    Update slack.sh

--- slack.sh
+++ slack.sh
@@ -4,16 +4,10 @@
 url=''
 username='app'
 
-## Values received by this script:
-# To = $1 (Slack channel or user to send the message to, specified in the Zabbix web interface; "@username" or "#channel")
-# Subject = $2 (usually either PROBLEM or RECOVERY)
-# Message = $3 (whatever message the Zabbix action sends, preferably something like "Zabbix server is unreachable for 5 minutes - Zabbix server (127.0.0.1)")
-
-# Get the Slack channel or user ($1) and Zabbix subject ($2 - hopefully either PROBLEM or RECOVERY)
 to="$1"
 subject="$2"
 
-# Change message emoji depending on the subject - smile (RECOVERY), frowning (PROBLEM), or ghost (for everything else)
+
 recoversub='^RECOVER(Y|ED)?$'
 if [[ "$subject" =~ ${recoversub} ]]; then
 	emoji=':smile:'
@@ -23,10 +17,9 @@ else
 	emoji=':ghost:'
 fi
 
-# The message that we want to send to Slack is the "subject" value ($2 / $subject - that we got earlier)
-#  followed by the message that Zabbix actually sent us ($3)
+
 message="${subject}: $3"
 
-# Build our JSON payload and send it as a POST request to the Slack incoming web-hook URL
+
 payload="payload={\"channel\": \"${to//\"/\\\"}\", \"username\": \"${username//\"/\\\"}\", \"text\": \"${message//\"/\\\"}\", \"icon_emoji\": \"${emoji}\"}"
 curl -m 5 --data-urlencode "${payload}" $url -A 'slack / https://github.com/rniceto/slack'
